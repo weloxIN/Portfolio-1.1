5dccc345a9b9df8826db04ad2050f5e84ee9d049
weloxIN <zxcmicrovawe87@gmail.com>
2026-06-29 01:08:00 +0000

    Main File

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import sys
+import os
 
 def count_words(text):
     words = text.split()
     return len(words)
 
 if __name__ == "__main__":
+    # демо-режим для CI/CD
     if len(sys.argv) > 1:
         text = " ".join(sys.argv[1:])
     else:
         text = sys.stdin.read().strip()
-    
+
     if not text:
-        print("Usage: echo 'some text' | python wordcounter.py")
-        sys.exit(1)
-    
+        text = "Docker DevOps Ansible Terraform Kubernetes DevSecOps"
+        print(f"Demo mode. Text: '{text}'")
+
     result = count_words(text)
     print(f"Word count: {result}")
